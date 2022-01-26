e5a0db0a052fd709dfb04c9581b2e267d2994a2e
Dirk Rudolph <drudolph@adobe.com>
2022-01-26 17:49:36 +0100

    add the new namespaces default sitemap extensions

--- testing/it/http/src/test/java/com/adobe/cq/wcm/core/components/it/http/SeoIT.java
+++ testing/it/http/src/test/java/com/adobe/cq/wcm/core/components/it/http/SeoIT.java
@@ -158,7 +158,9 @@ public class SeoIT {
             assertEquals(expectedSitemapIndex, index);
 
             String expectedSitemap = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-                + "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xhtml=\"http://www.w3.org/1999/xhtml\">"
+                + "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xhtml=\"http://www.w3.org/1999/xhtml\" "
+                + "xmlns:video=\"http://www.google.com/schemas/sitemap-video/1.1\" xmlns:image=\"http://www.google.com/schemas/sitemap-image/1.1\" "
+                + "xmlns:news=\"http://www.google.com/schemas/sitemap-news/0.9\">"
                 + "<url>"
                 + "<loc>http://integrationtest.local:" + publisherPort + cp + "gb/en.html</loc>"
                 + "<xhtml:link rel=\"alternate\" hreflang=\"en-GB\" href=\"http://integrationtest.local:" + publisherPort + cp + "gb/en.html\"/>"
